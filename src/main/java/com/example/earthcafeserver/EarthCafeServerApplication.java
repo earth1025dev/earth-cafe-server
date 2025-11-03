c36685af6c162d30e2d3e8c08b76ac5437a6a7ad
jisuuuu <gjkl55@naver.com>
2025-11-03 16:14:43 +0900

    init: 스프링부트 프로젝트 생성

---
+++ src/main/java/com/example/earthcafeserver/EarthCafeServerApplication.java
@@ -0,0 +1,11 @@
+package com.example.earthcafeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthCafeServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EarthCafeServerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/earthcafeserver/EarthCafeServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.earthcafeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthCafeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
